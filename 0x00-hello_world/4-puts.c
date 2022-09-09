e88250b65896f0c51027a86f153910dc634a2336
Lyton-Knight <debayqyibra@gmail.com>
2022-09-09 09:17:47 +0000

    Added 4-puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - print the string
+ *
+ * Description: using the main function
+ * The program prints "Programming is like buidling a multilingual puzzle
+ * Retun: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like buidling a multilingual puzzle");
+	return (0);
+}

6224030fae875ae9fac11f0246b151e7479a18d5
Lyton-Knight <debayqyibra@gmail.com>
2022-09-13 06:30:32 -0700

    Added file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *main - the function will print the string _putchar
+ * Return: 0
+ * */
+
+int main(){
+	char *var_ = "_putchar";
+	printf("%s \n", var_);
+
+	return 0 ;
+}

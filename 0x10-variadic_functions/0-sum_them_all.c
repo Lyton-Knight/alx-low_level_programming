7213c80a98968b88c3595be6d299e12ab97f9184
Lyton-Knight <debayqyibra@gmail.com>
2022-10-17 12:48:36 +0200

    Modified file 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,54 +1,25 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
- * sum_them_all - returns the sum of all parameters
- * @n: num of args
- * Return: sum of params
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	/* declarations */
-	unsigned int i;
-	int sum = 0;
-	va_list argsptr;
+	va_list nums;
+	unsigned int index, sum = 0;
 
-	va_start(argsptr, n);
-	/* check input */
-	if (n == 0)
-		return (0);
-	/* sum the parameters */
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(argsptr, int);
-	}
-	va_end(argsptr);
-	/* return sum */
-	return (sum);
-}
-#include "variadic_functions.h"
-#include <stdarg.h>
-/**
- * sum_them_all - returns the sum of all parameters
- * @n: num of args
- * Return: sum of params
- */
-int sum_them_all(const unsigned int n, ...)
-{
-	/* declarations */
-	unsigned int i;
-	int sum = 0;
-	va_list argsptr;
+	va_start(nums, n);
 
-	va_start(argsptr, n);
-	/* check input */
-	if (n == 0)
-		return (0);
-	/* sum the parameters */
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(argsptr, int);
-	}
-	va_end(argsptr);
-	/* return sum */
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+
+	va_end(nums);
+
 	return (sum);
 }
